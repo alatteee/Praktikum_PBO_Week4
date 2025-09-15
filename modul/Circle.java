43f305cf1e502953eb32573a15d2b673d9662511
Azkha Nazzala <azkhanazzala@gmail.com>
2025-09-15 14:10:48 +0700

    membuat cylinder. java dan melakukan test pada cylinder.java

--- modul/Circle.java
+++ modul/Circle.java
@@ -2,36 +2,50 @@
 * The Circle class models a circle with a radius and color.
 */
 public class Circle {
-    // private instance variable, not accessible from outside this class
     private double radius;
     private String color;
 
-    // Constructors (overloaded)
-    /** Constructs a Circle instance with default value for radius and color */
-    public Circle() { // 1st (default) constructor
-    radius = 1.0;
-    color = "red";
+    // Constructor default
+    public Circle() {
+        radius = 1.0;
+        color = "red";
     }
 
-    /** Constructs a Circle instance with the given radius and default color */
-    public Circle(double r) { // 2nd constructor
-    radius = r;
-    color = "red";
+    // Constructor dengan radius
+    public Circle(double r) {
+        radius = r;
+        color = "red";
     }
 
-    /** Returns the radius */
+    // Constructor baru (Task 1.1) dengan radius & color
+    public Circle(double r, String c) {
+        radius = r;
+        color = c;
+    }
+
+    // Getter & setter radius
     public double getRadius() {
-    return radius;
+        return radius;
+    }
+    public void setRadius(double r) {
+        radius = r;
     }
 
-    /** Returns the area of this Circle instance */
+    // Getter & setter color (Task 1.1)
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String c) {
+        color = c;
+    }
+
+    // Hitung luas lingkaran
     public double getArea() {
-    return radius*radius*Math.PI;
+        return radius * radius * Math.PI;
     }
 
-    /** Return a self-descriptive string of this instance in the form of
-    Circle[radius=?,color=?] */
+    // toString
     public String toString() {
-    return "Circle[radius=" + radius + " color=" + color + "]";
+        return "Circle[radius=" + radius + ", color=" + color + "]";
     }
 }
--- modul/TestCircle.java
+++ modul/TestCircle.java
@@ -1,8 +1,9 @@
 public class TestCircle {
     public static void main(String[] args) {
-        Circle c1 = new Circle();        // default: radius=1.0, color="red"
-        System.out.println(c1.toString()); // pakai toString bawaan modul
-        System.out.println("radius=" + c1.getRadius());
-        System.out.println("area=" + c1.getArea());
+        Circle c1 = new Circle(2.0, "blue"); // pakai constructor baru
+        System.out.println(c1.toString());   // Circle[radius=2.0, color=blue]
+
+        c1.setColor("green");                // pakai setter baru
+        System.out.println("New color: " + c1.getColor());
     }
 }
